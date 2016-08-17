9f1154668166d60baa23ff6416d8ef0a9f102415
Johann Dyck <contact@johanndyck.com>
2016-08-17 14:58:28 -0500

    added some imagery and fonts to design

--- footer.php
+++ footer.php
@@ -15,9 +15,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'interfacestudio' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'interfacestudio' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'interfacestudio' ), 'interfacestudio', '<a href="http://johanndyck.com" rel="designer">Johann Dyck</a>' ); ?>
+			&copy; <?php echo date("Y") ?> Johann Dyck.  All rights reserved.
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- front-page.php
+++ front-page.php
@@ -10,7 +10,7 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 	
 		<div class="lead">
-			<h2>Building Online Connection</h2>
+			<h2>Building Connection</h2>
 			<p>Our online lives are dominated by interfaces. We want to build the best interfaces that not only get the job done, but connect people to you. If you need help with your web project contact us to see how we can help.
 			</p>
 			<button>Contact</button>
@@ -20,17 +20,23 @@ get_header(); ?>
 		</div>
 		<section class="features">
 			<div class="feature">
-				<img src="Goals" alt="Goals">
+				<img src='<?php 
+					 echo get_template_directory_uri()
+					?>/images/strategy.svg' alt='Strategy' class="icon">
 				<h3>Online Strategy Consulting</h3>
 				<p>Does your site meet your business goals? We will work with you to connect your business strategy with your online presence.</p>
 			</div>
 			<div class="feature">
-				<img src="Interaction" alt="Interaction">
+				<img src='<?php 
+					 echo get_template_directory_uri()
+					?>/images/user.svg' alt='User' class="icon">
 				<h3>Creating Memorable Experiences</h3>
 				<p>Your user expects a great experience when visiting your site. We’ll make your site perform well, and look like you.</p>
 			</div>
 			<div class="feature">
-				<img src="Solutions" alt="Solutions">
+				<img src='<?php 
+					 echo get_template_directory_uri()
+					?>/images/maintenence.svg' alt='Maintenence' class="icon">
 				<h3>Maintainable Solutions</h3>
 				<p>Let’s work together to develop a solution that is not outdated as soon as it’s launched.</p>
 			</div>		
@@ -58,7 +64,21 @@ get_header(); ?>
 						 * If you want to override this in a child theme, then include a file
 						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 						 */
-						get_template_part( 'template-parts/content', get_post_format() );
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	
+
+	<div class="entry-content">
+		<?php
+			the_content();
+
+
+		?>
+	</div><!-- .entry-content -->
+
+</article><!-- #post-## --> 
+		<?php
 
 					endwhile;
 
