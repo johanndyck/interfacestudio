50437ae590e09dd92a63b268cd8789e8d49bfc97
Johann Dyck <contact@johanndyck.com>
2016-09-02 21:48:30 -0500

    more style changes

--- footer.php
+++ footer.php
@@ -19,11 +19,11 @@
 				&copy; <?php echo date("Y") ?> The Interface Studio.  All rights reserved.
 			</p>
 			<p>
-				The Interface Studio, LLC<br>
+				<i class="fa fa-envelope" aria-hidden="true"></i> The Interface Studio, LLC<br>
 				955 E Campbell Rd, Ste 300-541<br>
 				Richardson, TX 75081
 			</p>
-			<p><a href="tel:19723167333">(972)316-7333</p>
+			<p><i class="fa fa-phone" aria-hidden="true"></i> <a href="tel:19723167333">(972)316-7333</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
